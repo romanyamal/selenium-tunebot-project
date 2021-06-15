2122e1d1d2f03564eabf01a0e6be3df97807eb66
Roman Yamaletdinov <75280430+romanyamal@users.noreply.github.com>
2021-06-15 14:09:32 -0700

    Update out.py

--- out.py
+++ out.py
@@ -3,24 +3,30 @@ import requests
 import shutil
 import time
 import os
+ 
+#This file gets all the images from the saved urls from ebay and uses request to get url and download images and move them 
+#to proper folder
 
-DATA_FILE="partsmart15.json" 
-#DATA_FILE="sepsparts.json" 
+#Then for the next item it overwrites previous images and deletes the old extra images
 
-global img
-img=0
-source = r'C:\Users\sepsparts\Downloads\ebysave\image'
-destination = r'C:\Users\sepsparts\Downloads\ebysave\post_images\image'
+#Upon program ending with 'q' input, all photos are erased
+DATA_FILE="data.json" 
+
+global img, img_prev
+img =0
+img_prev=0
+source = r'C:\Users\username\Downloads\ebysave\image'
+destination = r'C:\Users\username\Downloads\ebysave\post_images\image'
 with open(DATA_FILE) as json_file:
     data = json.load(json_file)
     while 1:
         print("\n")
+        img_prev = img
         value=input("(Enter 'q' to quit) What index:")
         time.sleep(1)
-        if(img < 12 and img != 0):
-            for i in range(12-img):
-                os.remove(destination + str(img+i)+".jpg")
         if(value == 'q'):
+            for i in range(img_prev):
+                os.remove(destination + str(img_prev-i)+".jpg")
             break
         for i in range(len(data["ebay_list"])):
             if(int(value) == i):
@@ -41,3 +47,6 @@ with open(DATA_FILE) as json_file:
                             shutil.move(source + str(img)+".jpg", destination + str(img)+".jpg")
                     else:
                         break
+        if(img < 12 and img != img_prev):
+            for i in range(img_prev-img):
+                os.remove(destination + str(img+(abs(img_prev-img))-i)+".jpg")
